6e4838d6eeec45e63c4b10b1a9599cb5697690ab
Edwin18 <igreenbergi78@gmail.com>
2020-04-05 15:55:16 +0300

    Снова играюсь с табуляцией

--- js/checkers-board/checkers-board.ts
+++ js/checkers-board/checkers-board.ts
@@ -1,14 +1,14 @@
 class CheckersBoard {
-	private board: HTMLCanvasElement;
-	private ctx: CanvasRenderingContext2D;
-	private firstPlayer: Array < {} > ;
-	private secondPlayer: Array < {} > ;
+  private board: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+  private firstPlayer: Array < {} > ;
+  private secondPlayer: Array < {} > ;
 
-	constructor(firstPlayer, secondPlayer) {
-		this.board = document.querySelector(`#checkers`);
-		this.ctx = this.board.getContext(`2d`);
+  constructor(firstPlayer, secondPlayer) {
+    this.board = document.querySelector(`#checkers`);
+    this.ctx = this.board.getContext(`2d`);
 
-		this.firstPlayer = firstPlayer;
-		this.secondPlayer = secondPlayer;
-	}
+    this.firstPlayer = firstPlayer;
+    this.secondPlayer = secondPlayer;
+  }
 }
